6c1c9870446a6f933ea3d840e4a59f933ff2c096
Gabriel Khachatryan <gmkhach@gmail.com>
2018-11-26 09:23:41 -0500

    brute-force method

--- ReverseStringsInParentheses/Program.cs
+++ ReverseStringsInParentheses/Program.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿/*
+ * Write a C# program to reverse the strings contained in each pair of matching parentheses in a given string and also remove the parentheses within the given string.
+ * Example: "This (si) my (elpmaxe)." will return "This is my example."
+ */
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +15,66 @@ namespace ReverseStringsInParentheses
     {
         static void Main(string[] args)
         {
+            do
+            {
+                try
+                {
+                    Console.Write("\nInput a statements with two parentheses.\n\n>>> ");
+                    var input = Console.ReadLine();
+                    Console.WriteLine($"\n{ReverseStringsInParentheses(input)}");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("\n" + ex.Message);
+                }
+                Console.Write("\nPress Enter to do another merge...");
+                Console.ReadLine();
+                Console.Clear();
+            } while (true);
+        }
+
+        static string ReverseStringsInParentheses(string input)
+        {
+            List<string> segments = new List<string>();
+            string currentSegment = string.Empty;
+            string output = string.Empty;
+            
+            foreach(var ch in input)
+            {
+                if (ch == '(' || ch == ')')
+                {
+                    segments.Add(currentSegment);
+                    currentSegment = string.Empty;
+                }
+                else
+                {
+                    currentSegment += ch;
+                }
+            }
+            segments.Add(currentSegment);
+
+            Stack<char> myStack = new Stack<char>();
+
+            for (int i = 1; i < 4; i += 2)
+            {
+                foreach (var ch in segments[i])
+                {
+                    myStack.Push(ch);
+                }
+                segments[i] = string.Empty;
+                foreach (var ch in myStack)
+                {
+                    segments[i] += ch;
+                }
+                myStack.Clear();
+            }
+
+            foreach(var segment in segments)
+            {
+                output += segment;
+            }
+            return output;
+
         }
     }
 }
